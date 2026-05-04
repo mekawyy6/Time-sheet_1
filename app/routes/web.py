a670e39633316c1412cbcf062523ed027f4d4ec0
mekawyy6 <youssef.mekawy@vodafone.com>
2026-05-04 21:17:25 +0300

    Update web.py

--- app/routes/web.py
+++ app/routes/web.py
@@ -1302,7 +1302,7 @@ def manifest():
 @router.get("/sw.js")
 def sw():
     js = """
-const CACHE_NAME = 'field-timesheet-v4';
+const CACHE_NAME = 'field-timesheet-v6';
 const STATIC_URLS = [
   '/static/css/style.css'
 ];
